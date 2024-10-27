a6fb80a8a7df260947dbd0ed977a8baf218cd0af
Nikita Alexov <polarbeararmor@gmail.com>
2024-10-27 16:06:07 +0400

    feat: add jwt guard and update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from './auth/guards/jwt.guard';
+import { JwtStrategy } from './auth/strategy/jwt.strategy';
 
 @Module({
-  imports: [PostModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PostModule,
+    UserModule,
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard
+    },
+    JwtStrategy
+  ],
 })
 export class AppModule {}
